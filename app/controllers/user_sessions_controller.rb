27702c05190ff489f0c787b0c87bc0893229560a
Kiran Darisi <kiran.darisi@gmail.com>
2013-11-29 17:48:15 +0530

    Added compatibility with HMAC

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,3 +1,5 @@
+require 'openssl'
+
 class UserSessionsController < ApplicationController
   def new
     @user_session = UserSession.new
@@ -7,7 +9,8 @@ class UserSessionsController < ApplicationController
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
       flash[:notice] = "Successfully logged in."
-      redirect_url = AppConfig['freshdesk_domain_name']+"login/sso?name="+current_user.username+"&email="+current_user.email+"&hash="+gen_hash_from_params_hash
+      utctime = time_in_utc
+      redirect_url = AppConfig['freshdesk_domain_name']+"login/sso?name="+current_user.username+"&email="+current_user.email+"&timestamp="+utctime+"&hash="+gen_hash_from_params_hash(utctime)
       redirect_to redirect_url
     else
       puts @user_session.errors
@@ -15,8 +18,9 @@ class UserSessionsController < ApplicationController
     end
   end
   
-  def gen_hash_from_params_hash
-     Digest::MD5.hexdigest(current_user.username+current_user.email+AppConfig['freshdesk_sso_decret'])
+  def gen_hash_from_params_hash(utctime)
+    digest  = OpenSSL::Digest::Digest.new('MD5')
+     OpenSSL::HMAC.hexdigest(digest,AppConfig['freshdesk_sso_decret'],current_user.username+current_user.email+utctime)
   end
   
   def destroy
@@ -24,5 +28,10 @@ class UserSessionsController < ApplicationController
     @user_session.destroy
     flash[:notice] = "Successfully logged out."
     redirect_to root_url
+  end
+  private
+
+  def time_in_utc
+    Time.now.getutc.to_i.to_s
   end
 end
